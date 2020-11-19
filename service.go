58bec314c1a20c0da6a0dc15732a77f9ae8527d9
chenguofan1999 <chenguofan1999@outlook.com>
2020-11-19 22:11:49 +0800

    v0.0.16

--- service.go
+++ service.go
@@ -29,15 +29,7 @@ func NewServer() *negroni.Negroni {
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
-	fmt.Fprintln(w, `<p style="text-align: center;"><span style="font-family: Impact, Charcoal, sans-serif; font-size: 28px; color: rgb(40, 50, 78);">CloudGo</span></p>
-	<h3><strong><span style="color: rgb(71, 85, 119); font-size: 24px;">云服务(员)</span></strong></h3>
-	<div><span style="font-family: Verdana, Geneva, sans-serif; color: rgb(40, 50, 78); font-size: 18px;">./hello/[your name]</span></div>
-	<h3><strong><span style="color: rgb(71, 85, 119); font-size: 24px;">查询GPA功能😅</span></strong></h3>
-	<div><span style="font-size: 18px;"><span style="font-family: Verdana, Geneva, sans-serif; color: rgb(40, 50, 78);">./GPA/[your name]</span></span></div>
-	<h3><strong><span style="color: rgb(71, 85, 119); font-size: 24px;">爬虫功能</span></strong></h3>
-	<div><span style="font-family: Verdana, Geneva, sans-serif; color: rgb(40, 50, 78); font-size: 18px;">./crawl/?url=[your URL]</span></div>
-	<h3><strong><span style="color: rgb(71, 85, 119); font-size: 24px;">云计算(器)</span></strong></h3>
-	<div><span style="font-family: Verdana, Geneva, sans-serif; color: rgb(40, 50, 78); font-size: 18px;">./calc/{add/sub/mul/div}/?a=[?]&amp;b=[?]</span></div>`)
+	fmt.Fprintln(w, mainPageHTML)
 
 	for k, v := range r.Form {
 		fmt.Println(k, " : ", strings.Join(v, ""))
@@ -46,13 +38,13 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	fmt.Fprintln(w, "Hello, ", vars["name"])
+	fmt.Fprintln(w, "Hello,", vars["name"])
 }
 
 func gpaHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	n := 2 + 2*rand.Float32()
-	fmt.Fprintln(w, "GPA of ", vars["name"], " = ", n)
+	fmt.Fprintln(w, "GPA of", vars["name"], "is", n)
 }
 
 func crawl(w http.ResponseWriter, r *http.Request) {
