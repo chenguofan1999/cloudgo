2cd57a54902f1c18598a9d056e58a1e7db7eec1a
chenguofan1999 <chenguofan1999@outlook.com>
2020-11-19 22:00:42 +0800

    v0.0.15

--- service.go
+++ service.go
@@ -29,15 +29,15 @@ func NewServer() *negroni.Negroni {
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
-	fmt.Fprintln(w, `<p style="text-align: center;"><span style="font-family: Impact, Charcoal, sans-serif; font-size: 24px; color: rgb(40, 50, 78);">CloudGo</span></p>`)
-
-	fmt.Fprintln(w, `<h3><strong><span style="color: rgb(71, 85, 119);">测试功能</span></strong></h3>`)
-	fmt.Fprintln(w, `<div><span style='font-family: "Lucida Console", Monaco, monospace; color: rgb(40, 50, 78);'>1. /hello/[your name]</span></div>`)
-	fmt.Fprintln(w, `<div><span style='font-family: "Lucida Console", Monaco, monospace; color: rgb(40, 50, 78);'>2. /GPA/[your name]</span></div>`)
-	fmt.Fprintln(w, `<div><span style='font-family: "Lucida Console", Monaco, monospace; color: rgb(40, 50, 78);'>3. /crawl/?url=[your URL]</span></div>`)
-
-	fmt.Fprintln(w, `<h3><strong><span style="color: rgb(71, 85, 119);">云计算(器)</span></strong></h3>`)
-	fmt.Fprintln(w, `<div><span style='font-family: "Lucida Console", Monaco, monospace; color: rgb(40, 50, 78);'>4: /calc/{add/sub/mul/div}/?a=[?]&b=[?]</span></div>`)
+	fmt.Fprintln(w, `<p style="text-align: center;"><span style="font-family: Impact, Charcoal, sans-serif; font-size: 28px; color: rgb(40, 50, 78);">CloudGo</span></p>
+	<h3><strong><span style="color: rgb(71, 85, 119); font-size: 24px;">云服务(员)</span></strong></h3>
+	<div><span style="font-family: Verdana, Geneva, sans-serif; color: rgb(40, 50, 78); font-size: 18px;">./hello/[your name]</span></div>
+	<h3><strong><span style="color: rgb(71, 85, 119); font-size: 24px;">查询GPA功能😅</span></strong></h3>
+	<div><span style="font-size: 18px;"><span style="font-family: Verdana, Geneva, sans-serif; color: rgb(40, 50, 78);">./GPA/[your name]</span></span></div>
+	<h3><strong><span style="color: rgb(71, 85, 119); font-size: 24px;">爬虫功能</span></strong></h3>
+	<div><span style="font-family: Verdana, Geneva, sans-serif; color: rgb(40, 50, 78); font-size: 18px;">./crawl/?url=[your URL]</span></div>
+	<h3><strong><span style="color: rgb(71, 85, 119); font-size: 24px;">云计算(器)</span></strong></h3>
+	<div><span style="font-family: Verdana, Geneva, sans-serif; color: rgb(40, 50, 78); font-size: 18px;">./calc/{add/sub/mul/div}/?a=[?]&amp;b=[?]</span></div>`)
 
 	for k, v := range r.Form {
 		fmt.Println(k, " : ", strings.Join(v, ""))
