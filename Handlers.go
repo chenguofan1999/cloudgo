e387cb1d11d5787a57c79e153460a15e310cc412
chenguofan1999 <chenguofan1999@outlook.com>
2020-11-20 22:24:45 +0800

    aaa

--- Handlers.go
+++ Handlers.go
@@ -6,12 +6,9 @@ import (
 )
 
 func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, loginHTML)
 	fmt.Println("method:", r.Method) //获取请求的方法
-	if r.Method == "GET" {
-		// t, _ := template.ParseFiles("login.gtpl")
-		// log.Println(t.Execute(w, nil))
-		fmt.Fprintln(w, loginHTML)
-	} else {
+	if r.Method == "POST" {
 		//请求的是登录数据，那么执行登录的逻辑判断
 		r.ParseForm()
 		fmt.Println("username:", r.Form["username"])
