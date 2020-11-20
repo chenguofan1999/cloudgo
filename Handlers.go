b616a7feec76022018fa2daa8c31fd1f0f48e23a
chenguofan1999 <chenguofan1999@outlook.com>
2020-11-20 22:17:54 +0800

    input

--- Handlers.go
+++ Handlers.go
@@ -2,16 +2,15 @@ package cloudgo
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"text/template"
 )
 
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //获取请求的方法
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.gtpl")
-		log.Println(t.Execute(w, nil))
+		// t, _ := template.ParseFiles("login.gtpl")
+		// log.Println(t.Execute(w, nil))
+		fmt.Fprintln(w, loginHTML)
 	} else {
 		//请求的是登录数据，那么执行登录的逻辑判断
 		r.ParseForm()
